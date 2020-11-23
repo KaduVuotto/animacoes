14f5277b2da06a1086534232c5cf5b539cdb236c
CarlosAmorim <carlos.amorim@rckyinformatica.onmicrosoft.com>
2020-11-23 14:09:13 -0300

    alteracoes na animacao

--- App.js
+++ App.js
@@ -14,23 +14,66 @@ class App extends Component {
     this.state = {
       larAnimada: new Animated.Value(150),
       altAnimada: new Animated.Value(50),
+      opacidadeAnimada: new Animated.Value(0),
     };
 
-    Animated.timing(
+    Animated.sequence([
+      Animated.timing(
+        this.state.opacidadeAnimada,
+        {
+          toValue: 1,
+          duration: 1500,
+        }
+      ),
+      Animated.parallel([
+        Animated.timing(
+          this.state.larAnimada,
+          {
+            toValue: 300,
+            duration: 2000,
+          }
+        ),
+        Animated.timing(
+          this.state.altAnimada,
+          {
+            toValue: 200,
+            duration: 2000,
+          }
+        ),
+      ]),
+      Animated.timing(
+        this.state.opacidadeAnimada,
+        {
+          toValue: 0,
+          duration: 1500,
+        }
+      ),
+    ]).start()
+
+    /*Animated.sequence([
+      Animated.timing(
+        this.state.larAnimada,
+        {
+          toValue: 300,
+          duration: 2000,
+        }
+      ),
+      Animated.timing(
+        this.state.altAnimada,
+        {
+          toValue: 200,
+          duration: 2000,
+        }
+      ),
+    ]).start()*/
+
+    /*Animated.timing(
       this.state.larAnimada,
       {
         toValue: 300,
         duration: 2000,
       }
-    ).start();
-
-    Animated.timing(
-      this.state.altAnimada,
-      {
-        toValue: 300,
-        duration: 2000,
-      }
-    ).start();
+    ).start();*/
   }
 
   render() {
@@ -40,7 +83,8 @@ class App extends Component {
           width: this.state.larAnimada,
           height: this.state.altAnimada,
           backgroundColor: '#4169E1',
-          justifyContent: 'center'
+          justifyContent: 'center',
+          opacity: this.state.opacidadeAnimada,
         }}>
           <Text style={{
             color: 'white',
